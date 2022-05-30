ff7034876eea2ead18b27cbc0333f38a70b60ab0
Maryam Fazlali <fazlali1202@gmail.com>
2022-05-30 16:32:01 +0100

    Implemented all baseic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
+  root to: "jewels#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "jewel#index"
+  resources :jewels, except: %i[index] do
+    resources :bookings, only: %i[create]
+  end
+  resources :booking, only: %i[destroy]
 end
