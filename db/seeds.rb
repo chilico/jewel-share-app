50e1fe41e8480151ed3faa5dca1dd476c22f788c
chris owen <farawaychris.owen@gmail.com>
2022-05-31 14:27:18 +0100

    edited seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,14 @@
+puts "Creating users ..."
 5.times do
   user = User.new
   user.first_name = Faker::Name.first_name
   user.last_name = Faker::Name.last_name
   user.address = Faker::Address.street_address
   user.email = Faker::Internet.email
-  user.encrypted_password = Faker::Internet.password
+  password = Faker::Internet.password
+  user.password = password
+  user.password_confirmation = password
   user.save!
+  puts "Created user: #{user.first_name} #{user.last_name}"
 end
+puts "Successfully seeded database!"
