8872ef0963a5d25ac09f2ea1d78e9680110c85b8
chris owen <farawaychris.owen@gmail.com>
2022-06-03 15:53:13 +0100

    updtated review show

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -14,7 +14,7 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/pdunleav/cjofefl7u3j3e2sp0ylex3cyb"
     })
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
